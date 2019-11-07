f8a12653e989640e106f1443851cb519516b893f
julianmosi7 <moshammer.ju@gmail.com>
2019-11-07 20:10:01 +0100

    controls and model

---
+++ ModelLib/ValueChangedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib
+{
+    class ValueChangedEventArgs : EventArgs
+    {
+        public double Val { get; set; }
+    }
+}
---
+++ ModelLib/ValueModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ModelLib
+{
+    public class ValueModel
+    {
+        public event EventHandler<ValueChangedEventArgs> ValueChanged;
+
+        private double val;
+
+        public double Val
+        {
+            get => val;
+            set
+            {
+                val = value;
+                ValueChanged?.Invoke(this, new ValueChangedEventArgs { Val = val });
+            }
+        }
+
+    }
+}
